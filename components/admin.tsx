dbc1337c34eb1f2e3cd19bb585b07c208fddb742
Aman Yadav <amanyadav2303@gmail.com>
2026-05-11 01:40:37 +0530

    admin component fix

--- components/admin.tsx
+++ components/admin.tsx
@@ -1,13 +1,12 @@
-import { Outlet } from "react-router-dom";
-
-const Admin = () => {
+const Admin = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen bg-neutral-950 text-neutral-100 p-6">
       <div className="max-w-7xl mx-auto">
-        <Outlet />
+        {children}
       </div>
     </div>
   );
 };
 
 export default Admin;
+
